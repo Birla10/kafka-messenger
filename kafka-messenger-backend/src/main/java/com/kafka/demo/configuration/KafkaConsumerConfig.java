b644f173aeb1bab8b200939e8d45e791accf3aad
birla10 <birla.3@wright.edu>
2024-03-30 16:41:44 -0400

    Consumer and Producer configurations

---
+++ kafka-messenger-backend/src/main/java/com/kafka/demo/configuration/KafkaConsumerConfig.java
@@ -0,0 +1,38 @@
+package com.kafka.demo.configuration;
+
+import java.util.Collections;
+import java.util.Properties;
+
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class KafkaConsumerConfig {
+
+	@Value("${spring.kafka.bootstrap-servers}")
+	private String bootstrapServers;
+	
+	@Value("${spring.kafka.groupId}")
+	private String groupId;
+	
+	@Value("${spring.kafka.topic}")
+	private String topic;
+
+	@Bean
+	Consumer<String, String> kafkaConsumer() {
+		Properties properties = new Properties();
+		properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+		properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+		properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+		properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+		
+		Consumer<String, String> consumer = new KafkaConsumer<>(properties);
+		consumer.subscribe(Collections.singletonList("messenger"));
+		return consumer;
+	}
+}
---
+++ kafka-messenger-backend/src/main/java/com/kafka/demo/configuration/KafkaProducerConfig.java
@@ -0,0 +1,29 @@
+package com.kafka.demo.configuration;
+
+import java.util.Properties;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class KafkaProducerConfig {
+	
+	@Value("${spring.kafka.bootstrap-servers}")
+	private String bootstrapServers;
+
+	@Bean
+	Producer<String, String> kafkaProducer() {
+		Properties properties = new Properties();
+		properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+		properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+		properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+		return new KafkaProducer<>(properties);
+	}
+
+}
